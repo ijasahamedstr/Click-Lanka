f72799797bf10b1ed17bcb8085dbbfb489bcaad1
ijasahamedstr <ijasahamed.str@gmail.com>
2025-03-10 12:46:21 +0200

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct paths for assets
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-});
+})
